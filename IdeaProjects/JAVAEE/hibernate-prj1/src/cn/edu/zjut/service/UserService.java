f8cda5ef04db3a3037b71f2c0469e75aaa45e658
shk314159 <50333579+shk314159@users.noreply.github.com>
2020-11-11 13:19:46 +0800

    123

---
+++ IdeaProjects/JAVAEE/hibernate-prj1/src/cn/edu/zjut/service/UserService.java
@@ -0,0 +1,18 @@
+package cn.edu.zjut.service;
+import java.util.List;
+import cn.edu.zjut.po.Customer;
+import cn.edu.zjut.dao.CustomerDAO;
+public class UserService {
+    public boolean login(Customer loginUser) {
+        String account = loginUser.getAccount();
+        String password = loginUser.getPassword();
+        String hql = "from Customer as user where account='"
+                +account+ "' and password='" + password +"'";
+        CustomerDAO dao = new CustomerDAO();
+        List list = dao.findByHql(hql);
+        if(list.isEmpty())
+            return false;
+        else
+            return true;
+    }
+}
